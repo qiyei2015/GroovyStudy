138fb4ed99e5e25238617f6308db2fbaeb947607
qiyei2015 <1273482124@qq.com>
2020-02-02 23:32:21 +0800

    初始化工程

---
+++ src/HelloGroovy.groovy
@@ -0,0 +1,11 @@
+/**
+ * @author Created by qiyei2015 on 2020/2/2.
+ * @version: 1.0* @email: 1273482124@qq.com
+ * @description:
+ */
+class HelloGroovy {
+
+    public static void main(String[] args) {
+        println "hello groovy"
+    }
+}
